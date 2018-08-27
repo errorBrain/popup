a2c4c8a58ed8bc76dfbe8d25526c5ae82def7f01
errorBrain <41650100+errorBrain@users.noreply.github.com>
2018-08-28 02:56:54 +0700

    Update pop.js

--- pop.js
+++ pop.js
@@ -3,7 +3,7 @@ var PopWidth = 1370;
 var PopHeight = 800;
 var PopFocus = 0;
 var _Top = null;
-var hitadsmedia = 'https://bit.ly/termuxtutorial';
+var hitadsmedia = 'http://bit.ly/termuxtutorial';
 
 function GetWindowHeight() {
   var myHeight = 0;
