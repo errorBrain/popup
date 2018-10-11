c9cf2324857e5152e4a0940d3cfb0bc8f9094d1b
errorBrain <41650100+errorBrain@users.noreply.github.com>
2018-10-11 20:32:29 +0700

    Update Auto.js

--- Auto.js
+++ Auto.js
@@ -2,7 +2,7 @@ function extractDomain(url) {
 	var hostname;
 	if (url.indexOf("://") > -1) {hostname = url.split('/')[2];}
 	else {hostname = url.split('/')[0];}
-	hostname = hostname.split(':')[0];
+	hostname = hostname.split('s:')[0];
 	hostname = hostname.split('?')[0];
 	return hostname;
 }
